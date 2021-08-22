8ecf7c5aa6bbd117f01392ec274795224ce2bcfe
donyiezeng <46657489+donyiezeng@users.noreply.github.com>
2021-08-22 02:09:25 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,172 @@
+joe = True
+aaaaa = 0
+while True:
+  ans = " "
+  if aaaaa > 0:
+    print("Are you stupid, read the prompt dumbdumb")
+  print("Introducing 'Hi Hungry', the newest way to solve your 'I don't know what to eat tonight' problem.")
+  print("Hello there, are you ready to be amazed? (y/n)")
+  ans = input()
+  if ans == "y":
+    print("Are we in a simulation? (y/n)")
+    ans = input()
+    if ans == "y":
+      print("Are you a stranger to love? (y/n)")
+      ans = input()
+      if ans == "y":
+        print("If you try to fail and you succeed did you just fail (f) or succeed (s)" )
+        ans = input("")
+        if ans == "s":
+          print("Chick Fil-A, yes or no (y/n)")
+          ans = input()
+          if ans == "y":
+            print("Is a sauce considered a soup? (y/n)")
+            ans = input()
+            if ans == "y":
+              print("Punishable by death, however we shall take pity on you and give you some of this so called soup. YOU ARE SENTENCED TO MAYONNAISE, also u support a homophobic company, kinda rude")
+              break
+            elif ans == "n":
+              print("Ah yes, a man/woman/non-binary (even though you support a homophobic company) of culture. Good job, now we shall grant you a bliss with this so called soup. You shall slurp lentil soup mortal")
+              break 
+          elif ans == "n":
+            print("Do you know the muffin man? (y/n)")
+            ans = input()
+            if ans == "y":
+              print("Wow, you know the muffin man???? What an honor, I'm sure he would be super willing to give you a piece of his soul. Here a blueberry muffin, also good for you for not supporting a homophobic company")  
+              break
+            elif ans == "n":
+              print("WTFrick, muffin man is so flippen disappointed (am i allowed to swear?) forking shirtballs. Anyways, here's a chocolate cupcake.")
+              break
+            
+        elif ans == "f":
+          print("Is water wet? (y/n)")
+          ans = input()
+          if ans == "y":
+            print("Do donuts = bagels? (y/n)")
+            ans == input()
+            if ans == "y":
+              print("Microwaved lasagna is all you deserve, you lazy rat, go eat some at the subway of New York")
+              break
+            elif ans == "n":
+              print("You are able to differentiate bagels and donuts???? wow congrats you pretencious birch. Have yourself some red wine and cheese")
+              break
+          elif ans == "n":
+            print("Would it be ethical to go back in time and kill baby Stalin? (y/n)")
+            ans=input()
+            if ans == "y":
+              print("You have no morals whatsoever, please may fried cheese be the only thing you eat for the rest of your life until you're behind bars")
+              break
+            elif ans == "n":
+              print("okay cool,, im worried,, but you're okay---- heres a corndog????")
+              break
+ 
+      elif ans == "n":
+        print("Do you know the rules? (y/n)") 
+        ans = input() 
+        if ans == "y": 
+          print("Are you thinking of a full commitment? (y/n)") 
+          ans = input()
+          if ans == "y":
+            print("Would you get this from any other guy? (y/n)")
+            ans = input()
+            if ans == "y":
+              print("Can you get enough of something? Yes, the answer is yes, so shawarma, something that you can only eat once a week")
+              break
+            if ans == "n":  
+              print("I JUST WANNA TELL YOU HOW IM FEELING, GOTTA MAKE YOU UNDERSTAND, sausage rolls. Get it. Like rick sausage rolls. so funny")
+              break
+          elif ans == "n":
+            print("Was the moonlanding real? (y/n)")
+            ans = input()
+            if ans == "y":
+              print("A non commitment type of person, yet with class. Chicken nuggies for you")
+              break
+            elif ans == "n":
+              print("A non commitment type of person. But you lack class. Chicken tendies for you >:(")
+              break
+        elif ans == "n":
+          print("Are you happy? (y/n)")
+          ans = input()
+          if ans == "y":
+            print("Do you train your dragon (1) or does the dragon train you (2)?")
+            ans = input()
+            if ans == "1":
+              print("Your dream food at this moment is rotisserie chicken, enjoy you cool viking")
+              break
+            if ans == "2":
+              print("You're a caesar salad typa guy, like a lame viking, eating salad")
+              break
+          elif ans == "n":
+              print("Did po become the dragon warrior through fate (1) or did his training make him the dragon warrior (2)")
+              ans = input()           
+              if ans == "1":
+                print("If you believe so, you'll never know the secret ingredient. Instant noodles for you")
+                break
+              elif ans == "2":
+                print("Quit, dont quit, noodles, dont noodles. There is no secret ingredient, maybe the secret ingredient was the friends that we made along the way. Secret ingredient soup for you")
+                break
+                
+    elif ans == "n":
+      print("Are you team Edward (1) or team Jacob (2)")
+      ans=input()
+      if ans == "1":
+        print("Are humans donut shaped? (y/n)")
+        ans=input()
+        if ans == "y":
+          print("Is a hotdog a sandwich? (y/n)")
+          ans=input()
+          if ans =="y":
+            print("Do you believe in bees? (y/n)")
+            ans=input()
+            if ans =="y":
+              print("Go eat ratatouille, and no ratatouille is not the name of the rat why does everyone get it wrong. You deserve some decent food your answers were up to standard. Don't go eat a rat. Unless you want to. If you do I hope you feel me judging you through the screen.")
+              break
+            elif ans =="n":
+              print("Go eat honey mustard sauce. The whole bottle. How do you not bee-lieve in bees? Haha get it? I'll let myself out now. But seriously how can you think humans are donut shaped and that hotdogs are sandwiches and NOT believe in bees.")
+              break
+          elif ans =="n":
+            print("Do you know candice? (y/n)")
+            ans=input()
+            if ans == "y":
+              print("I'm impressed..you didn't fall for it. But because of that go eat some ketchup on pizza. I havent tried it but I'm assuming its bad. Its more fun for us when people fall for it.")
+              break
+            elif ans =="n":
+              print("Nuts.")
+              break
+        elif ans == "n":
+          print("Do you think pineapple belongs on pizza? (y/n)")
+          ans=input()
+          if ans == "y":
+            print("Would you rather eat sushi (1) or nothing (2)")
+            ans=input()
+            if ans == "1":
+              print("Too bad you get pickle juice.")
+              break
+            elif ans == "2":
+              print("How do you not want sushi. Go eat some sushi. Also how do you not want sushi and also think pineapple belongs on pizza. Please go get the help you need I'm worried about you.")
+              break
+          elif ans =="n":
+            print("If I hit you with a dictionary would it be verbal abuse (1) or physical assault (2)?")
+            ans=input()
+            if ans == "1":
+              print("You got the joke! As a reward have a good taco whatever day it is. But as a side note humans are most definitely donut shaped.")
+              break
+            elif ans == "2":
+              print("That joke flew right over your head didnt it? As a punishment go eat some pineapple on pizza. I know you told me you didn't like pineapple on pizza. You're welcome.")
+              break
+      elif ans =="2":
+        print("Are you antivax? (y/n)")
+        ans=input()
+        if ans == "y":
+          print("You get nothing you don't deserve it. Stop being selfish and ignorant and go read some scientific journals for me and come back when you've changed your mind. We don't tolerate antivaxxers here.")
+          break
+        elif ans == "n":
+          print("Are you sure you're not in a dream right now? (y/n)")
+          ans=input()
+          if ans == "y":
+            print("You get sliced bread. You're pretty in touch with reality. And who doesn't love bread.")
+            break
+          elif ans == "n":
+            print("You're correct, good job. You're one of the few who have passed the test. Welcome to the dream state. You'll be staying here for the rest of your 'life' as you know it. Here's some cotton candy to help you pass some time. You'll only be here for a couple trillion years and I'll be coming back to get you in no time to bring you to the next level.")
+            break      
+  aaaaa += 1
